b68c2dfa8a506de83267ea60158e1f35fe939ca6
3rdflr <3rdflrhtl@gmail.com>
2025-12-28 19:14:49 +0900

    fix: change Script loading strategy to 'afterInteractive' for improved performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -113,7 +113,7 @@ export default function RootLayout({
             }),
           }}
         />
-        <Script src={API} strategy='beforeInteractive' />
+        <Script src={API} strategy='afterInteractive' />
         <Script
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_ID}`}
